adcef5fd8ebe508d20d63d6bc4dfe37675fcfeb1
Paul DAO <dao.van.paul@gmail.com>
2020-03-23 15:01:07 +0100

    css

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,20 +12,21 @@ function Contact() {
     return (
         <div className="Contact">
             <img src={avatar} alt="avatar" className="avatar"/>
-            <p className="name">{name.firstName + ' ' + name.lastName}</p>
-            <br/>
-            <div className="status">
-                {status ? 
-                    <div>
-                        <div className="status-online"></div>
-                        <p className="status-text">online</p>
-                    </div>
-                    :
-                    <div>
-                        <div className="status-offline"></div>
-                        <p className="status-text">offline</p>
-                    </div>
-                }
+            <div className="info">
+                <p className="name">{name.firstName + ' ' + name.lastName}</p>
+                <div className="status">
+                    {status ? 
+                        <div>
+                            <div className="status-online"></div>
+                            <p className="status-text">online</p>
+                        </div>
+                        :
+                        <div>
+                            <div className="status-offline"></div>
+                            <p className="status-text">offline</p>
+                        </div>
+                    }
+                </div>
             </div>
         </div>
     )
