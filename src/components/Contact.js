cdbc50d77d74cb3c67234947b808b457ea6676d0
Paul DAO <dao.van.paul@gmail.com>
2020-03-24 13:01:17 +0100

    add setState of online status

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,36 +1,41 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import './style/Contact.css';
 
+class Contact extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            online: this.props.online
+        }
+    }
 
-Contact.prototype = {
-    name: PropTypes.string.isRequired,
-    avatar: PropTypes.string.isRequired,
-    online: PropTypes.bool.isRequired
-}
-
-function Contact(props) {
-    return (
-        <div className="Contact">
-            <img src={props.avatar} alt="props.avatar" className="avatar"/>
-            <div className="info">
-                <p className="name">{props.name}</p>
-                <div className="status">
-                    {props.online ? 
-                        <div>
-                            <div className="status-online"></div>
-                            <p className="status-text">online</p>
-                        </div>
-                        :
-                        <div>
-                            <div className="status-offline"></div>
-                            <p className="status-text">offline</p>
-                        </div>
-                    }
+    render() {
+        return (
+            <div className="Contact">
+                <img src={this.props.avatar} alt="this.props.avatar" className="avatar"/>
+                <div className="info">
+                    <p className="name">{this.props.name}</p>
+                    <div className="status" onClick={event => {
+                        this.setState({online : !this.state.online})
+                    }}>
+                        {this.state.online ? 
+                            <div>
+                                <div className="status-online"></div>
+                                <p className="status-text">online</p>
+                            </div>
+                            :
+                            <div>
+                                <div className="status-offline"></div>
+                                <p className="status-text">offline</p>
+                            </div>
+                        }
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
+    
 }
-
+        
+        
 export default Contact;
