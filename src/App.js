cb0f83456103c47e484604b322b094d66b4dd4f4
Paul DAO <dao.van.paul@gmail.com>
2020-03-24 10:38:48 +0100

    add proptype to param component Contact

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Contact from './components/Contact';
 
+
+const contact1 = {firstName: 'Leona', lastName:'Garett'};
+const contact2 = {firstName: 'Glenda', lastName:'Oliver'};
+const contact3 = {firstName: 'John', lastName:'Willis'};
 function App() {
   return (
     <div className="App">
-      <Contact />
+      <Contact name={contact1} avatar="https://randomuser.me/api/portraits/women/29.jpg" status/>
+      <Contact name={contact2} avatar="https://randomuser.me/api/portraits/women/21.jpg" status/>
+      <Contact name={contact3} avatar="https://randomuser.me/api/portraits/men/30.jpg"/>
     </div>
   );
 }
-
 export default App;
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,21 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './style/Contact.css';
 
-const name = {
-    firstName: 'Leona',
-    lastName: 'Garret'
-}
-const avatar = "https://randomuser.me/api/portraits/women/29.jpg";
-const status = true;
 
-function Contact() {
+Contact.prototype = {
+    name: PropTypes.objectOf(PropTypes.string),
+    avatar: PropTypes.string.isRequired,
+    status: PropTypes.bool.isRequired
+}
+
+function Contact(props) {
     return (
         <div className="Contact">
-            <img src={avatar} alt="avatar" className="avatar"/>
+            <img src={props.avatar} alt="props.avatar" className="avatar"/>
             <div className="info">
-                <p className="name">{name.firstName + ' ' + name.lastName}</p>
+                <p className="name">{props.name.firstName + ' ' + props.name.lastName}</p>
                 <div className="status">
-                    {status ? 
+                    {props.status ? 
                         <div>
                             <div className="status-online"></div>
                             <p className="status-text">online</p>
